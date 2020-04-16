dcd74e0967e87d887bd2cceec171ef0746b706ea
Nyira Mwalila <mwalilanyira@yahoo.com>
2020-04-16 14:33:04 +0200

    congiguration of routes in express

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
+//import routes
+const adminRoutes = require("./routes/admin/admin");
+const clientRoutes = require("./routes/client/shop");
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use("/add-product", (req, res, next) => {
-  res.send("Add product");
-});
-app.use("/message", (req, res, next) => {
-  res.send("<h1>Message from express</h1>");
-});
-app.use("/", (req, res, next) => res.send("Hello from express"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//register routes
+app.use(adminRoutes);
+app.use(clientRoutes);
 
 //create server
 app.listen(PORT, () => {
